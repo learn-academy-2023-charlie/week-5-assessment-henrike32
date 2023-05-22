5521994cf3507fff0c839d9744aeda5d93be708e
henrike32 <henrique.lambertucci@gmail.com>
2023-05-21 20:03:39 -0400

    did r3 and js1, working on 2 and 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,40 +13,124 @@
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
+describe("gotString", () => {
+    it(" takes in a string and returns a string with a coded message ", () => {
+        const secretCodeWord1 = "Lackadaisical"
+        // Expected output: "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        // Expected output: "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        // Expected output: "3cc3ntr1c"
+        expect(gotString(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(gotString(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(gotString(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
-
+// ReferenceError: gotString is not defined
 // b) Create the function that makes the test pass.
+// pseudocode - 
+// output -> takes in a string
+// input -> returns a string with a coded message which converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
+// process -
+// create a function named 'gotString'
+// using the .replaceAll method we can replace the given letters/integers in the original string ->  str1.replace(regexp, str2)
+// the callback function (match) => codeMap[match] is used to change each matched letter to its corresponding code from the codeMap object
+// Method 1: 
+const secretCodeWord1 = "Lackadaisical"
+const secretCodeWord2 = "Gobbledygook"
+const secretCodeWord3 = "Eccentric"
+
+const gotString = (string) => {
+    const codeMap = {
+      a: '4',
+      e: '3',
+      E: '3',
+      i: '1',
+      o: '0',
+    };
+    return string.replace(/[aEeio]/g, (match) => codeMap[match])
+}
+console.log(gotString(secretCodeWord1))
+console.log(gotString(secretCodeWord2))
+console.log(gotString(secretCodeWord3))
+
+// Method 2:
+// const secretCodeWord1 = "Lackadaisical"
+// const secretCodeWord2 = "Gobbledygook"
+// const secretCodeWord3 = "Eccentric"
+// const gotString =  (string) => {
+//     let newStr = string.replace(/[aeio]/g, (match) => {
+//         switch (match) {
+//             case 'a':
+//               return '4'
+//             case 'e':
+//               return '3'
+//             case 'E':
+//               return '3'
+//             case 'i':
+//               return '1'
+//             case 'o':
+//               return '0'
+//         }
+//     })
+//     return newStr
+// }
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
-
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const filterLetterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const filterLetterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
+describe('filterArray', () => {
+    it("should filter the array based on the given letter", () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const filterLetterA = "a"
+        const filterLetterE = "e"
+        
+        expect(filterArray(fruitArray, filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        
+        expect(filterArray(fruitArray, filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+    })
+});
 
 // b) Create the function that makes the test pass.
+// pseudocode -
+// input -> array containing strings
+// output -> array containing specific letter in their strings
+// process -
+
+// create a function named 'filterArray'
+const filterArray = (arr, letter) => {
+// create a variable to store the new array with the expected letters
+let newArray = [];
+
+    // use a for loop to iterate through the array
+    for (let i=0; i < arr.length; i++){
+        // locate the letter we want using .includes
+        if (arr[i].includes(letter)){
+        // use .push to move the matching letters strings into a new array
+        newArray.push(arr[i]);
+        }
+    }
+    return newArray;
+};
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
+// pseudocode ->
+// input ->
+// output ->
+// process ->
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
+// const hand1 = [5, 5, 5, 3, 3]
+// // Expected output: true
+// const hand2 = [5, 5, 3, 3, 4]
+// // Expected output: false
+// const hand3 = [5, 5, 5, 5, 4]
+// // Expected output: false
+// const hand4 = [7, 2, 7, 2, 7]
+// // Expected output: true
 
 // b) Create the function that makes the test pass.
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -2,8 +2,8 @@
 # MINASWAN
 
 # --------------------1) Create a method that takes in an array of words and a single letter and returns an array of all the words containing that particular letter. Use the test variables provided.
-beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
-filter_letter_o = 'o'
+# beverages_array = ['coffee', 'tea', 'juice', 'water', 'soda water']
+# filter_letter_o = 'o'
 # Expected output: ['coffee', 'soda water']
 # filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
@@ -21,20 +21,52 @@ filter_letter_o = 'o'
 # result_array = us_states.values.to_a.flatten.sort    
 # gives me an output of => ["Arizona", "California", "Idaho", "Maine", "Nevada", "New Hampshire", "Oregon", "Rhode Island", "Washington"] 
 
-def extract_val(hash)
-    values = hash.values.flatten.sort
-end
-us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], northeast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+# def extract_val(hash)
+    # values = hash.values.flatten.sort
+# end
+# us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], northeast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
-result = extract_val(us_states)
+# result = extract_val(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+class Bike 
+    attr_accessor :model, :wheels, :current_speed
+    def initialize (model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
 
+    end
 
-# -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+    def bike_info
+        "The #{:model} has #{:wheels} and is going #{:current_speed} mph."
+    end
+    # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
+    def pedal_faster(current_speed)
+        # had to find a way to increase the initial speed and maintain the value, didn't knew if I could use 'Integer' as a incremental value, tried this out and it worked! Happy to actually do some code without referring to chatGPT or google.
+        @current_speed =  @current_speed + current_speed
+    end 
+
+    def pedal_brake(break_speed)
+        # in this scenario you want the speed to reduce but without going to zero, so I did a conditional statement to stop the operation when the speed would go to less than zero.
+        @current_speed = @current_speed - break_speed 
+# Didn't figured it out why I am getting ' 10 ,28 , nil , "negative speed doesn't exist" ' as an output instead of getting 23 I am getting nil, without lines of code 57-59 the code gives me the correct expected values except for the -2 one. Since I still have a lot to cover in this assessment I will keep pressing forward.
+        # if @current_speed < 0
+        #     "Negative speed doesn't exist"
+        # end
+    end
+end
+
+my_bike = Bike.new('Trek Bike') 
+# => #<Bike:0x00000001035af4c0 @model="Trek Bike", @wheels=2, @current_speed=0>
+# Like I learned during the last week assessment-review we should remove our puts commands, but since I didn't managed to complete it properly, this time I will leave it for troubleshooting purposes.
+p my_bike.pedal_faster(10)
+p my_bike.pedal_faster(18)
+p my_bike.pedal_brake(5)
+p my_bike.pedal_brake(25)
 
 # Expected output example: my_bike.pedal_faster(10) => 10
 # Expected output example: my_bike.pedal_faster(18) => 28
